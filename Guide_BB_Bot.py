c1c4d85bc3ead40e9fe46de61b58c325c5c941c9
Olga <92888189+OlgaVetenkova@users.noreply.github.com>
2021-12-10 21:06:15 +0300

    Add files via upload

--- Guide_BB_Bot.py
+++ Guide_BB_Bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)    #Телеграм позволяет автоматически уменьшить размер, для этого необходимо передать в инициализатор класса ReplyKeyboardMarkup параметру resize_keyboard значение True
     button_1 = types.KeyboardButton('О компании')
     button_2 = types.KeyboardButton('Заказ канц.товаров')
     button_3 = types.KeyboardButton('Время работы')
@@ -29,13 +29,13 @@ def start(message):
     bot.send_message(message.chat.id,
                          "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный помочь тебе разобраться как тут всё устроено.".format(
                              message.from_user, bot.get_me()),
-                         parse_mode='html', reply_markup=markup)
+                         parse_mode='html', reply_markup=markup)  # reply_markup - нужно чтобы вывести меню нашему пользователю (модуль для преобрахования языка html) и передаём на созданный markup
 
-    @bot.message_handler(content_types=['text'])
+    @bot.message_handler(content_types=['text'])  #обработчик событий
     def bot_message(message):
-        if message.chat.type == 'private':
+        if message.chat.type == 'private':   # private - значит является личным сообщением, а не, например, телеграм-каналом
             if message.text == 'О компании':
-                bot.send_message(message.chat.id, 'Информацию о Компании можешь посмотреть здесь: https://bbrovar.by/ru/about-us/')
+                bot.send_message(message.chat.id, 'Информацию о Компании можешь посмотреть здесь: https://bbrovar.by/ru/about-us/')  #message.chat.id – идентификатор чата
             elif message.text == 'Заказ канц.товаров':
                 bot.send_message(message.chat.id, 'Подойди на ресепшн. Наш офис-менеджер Виктория даст тебе всё необходимое для работы.')
             elif message.text == 'Время работы':
@@ -53,9 +53,20 @@ def start(message):
             elif message.text == 'Бронь переговорки':
                 bot.send_message(message.chat.id, 'Для брони комнаты переговоров пройди по ссылке: 'f'{config.MEETING_ROOM}')
             elif message.text.lower() == 'привет':
-                bot.send_message(message.chat.id, 'Привет :) нажми на кнопку меню справа в строке ввода сообщений.')
+                #bot.send_message(message.chat.id, 'Привет :) нажми на кнопку меню справа в строке ввода сообщений.')
+                bot.send_video(message.chat.id, 'https://i.gifer.com/DSt1.gif', None, 'Хэй! Привет :) нажми на кнопку меню справа в строке ввода сообщений.')
             else:
                 bot.send_message(message.chat.id, ':) Давай попробуем ещё раз - нажми на кнопку меню справа в строке ввода сообщений.')
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True)  #чтобы бот не отключался
+
+
+# Добавить кнопки/ если упоминается какое-то слово if x in message/ разные виды приветствий/ добавить бобра + стикеры (возможно свои)
+
+# Декоратор @message_handler реагирует на входящие сообщение.
+# Message – это объект из Bot API, содержащий в себе информацию о сообщении. Полезные поля:
+# message.chat.id – идентификатор чата
+# message.from.id – идентификатор пользователя
+# message.text – текст сообщения
+# Функция send_message принимает идентификатор чата (берем его из сообщения) и текст для отправки.
